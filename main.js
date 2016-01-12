1a6979086f2e7af684cd47770fa7ed63d2607400
arnoldbaghom <arnoldbaghomia@gmail.com>
2016-01-12 01:31:27 -0800

    updated logic

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 var x = true;
 var o = false;
 
-
-
 $(document).ready(init);
 
 function init(){
@@ -10,9 +8,7 @@ function init(){
     $('.box').on('click', turnHandler)
   
     var x = true;
-
 }
-
 function turnHandler(event){
  	 if (x===true){
  	 	console.log("working inside x")
@@ -27,26 +23,85 @@ function turnHandler(event){
 	 	if($(this).text()!=='X' && $(this).text() === ''){
 		$(this).text('O')
 		$('.turn').text("X's");
-		  x = true;
-		
+		  x = true;	
 	}
 }
  winner();
 }
 
-function winner(){
-         
-         if($('#square1').text()   ===  $('#square2').text() && $('#square2').text() !== '' && $('#square2').text() === $('#sqaure3').text()) {alert($('#square1').text() + "Are A Winner !!! ")}
-         else if ($('#square1').text() === $('#square4').text() && $('#square4').text() !== '' && $('#square4').text() === $('#square7').text()) {alert($('#square4').text() + "Are A Winner !!! ")}
-         else if ($('#square1').text() === $('#square5').text() && $('#square5').text() !== '' && $('#square5').text() === $('#square9').text()) {alert($('#square5').text() + "Are A Winner !!! ")}
-         else if ($('#square4').text() === $('#square5').text() && $('#square5').text() !== '' && $('#square5').text() === $('#square6').text()) {alert($('#square5').text() + "Are A Winner !!! ")}
-         else if ($('#square7').text() === $('#square8').text() && $('#square8').text() !== '' && $('#square8').text() === $('#square9').text()) {alert($('#square9').text() + "Are A Winner !!! ")}
-         else if ($('#square2').text() === $('#square5').text() && $('#square5').text() !== '' && $('#square5').text() === $('#square8').text()) {alert($('#square8').text() + "Are A Winner !!! ")}
-         else if ($('#square3').text() === $('#square6').text() && $('#square6').text() !== '' && $('#square6').text() === $('#square9').text()) {alert($('#square8').text() + "Are A Winner !!! ")}
-         else if ($('#square3').text() === $('#square5').text() && $('#square5').text() !== '' && $('#square5').text() === $('#square7').text()) {alert($('#square7').text() + "Are A Winner !!! ")}
-   }
+function winner()
+{
+	squares = squareMatrix();
 
+	if (rowWin(squares) === 3 ||colWin(squares) === 3 ||diagWin(squares) === 3)
+		alert("X Wins !!! ");
+	else if (rowWin(squares) === -3 ||colWin(squares) === -3 ||diagWin(squares) === -3)
+		alert("O Wins !!! ");
+};
 
+function squareMatrix() {
 
+	squares = [];  // an array of 3 arrays
+	$(".row").each(function()
+	{
+		row = [];   // an array of 3 numbers
+		$(this).children(".box").each(function()
+		{
+			val = 0;
+			if ($(this).text() === 'X') val = 1;
+			if ($(this).text() === 'O') val = -1;
+			row.push(val);  
+		})
+		squares.push(row);  // add this array of three numbers to the array of 3 arrays
+	})
+	return squares;   // squares is a 3x3 matrix, an array of arrays
+}
+
+function rowWin(squares)
+{
+
+	for (i = 0; i < 3; ++i) {
+		sum = 0;
+
+		for (j = 0; j < 3; ++j)
+			sum += squares[i][j];
+		if (Math.abs(sum) === 3)
+			return sum;
+	}
+
+    
+   return 0;
+}
+
+function colWin(squares)
+{
+	for (i = 0; i < 3; ++i) {
+		sum = 0;
+		for (j = 0; j < 3; ++j)
+			sum += squares[j][i];
+		if (Math.abs(sum) === 3)
+			return sum;
+	}
+
+	return 0;
+}
+
+function diagWin(squares)
+{
+	sum = 0;
+	for (i = 0; i < 3; ++i) {
+			sum += squares[i][i];
+		}
+	if (Math.abs(sum) === 3)
+			return sum;
+	sum = 0;
+	for (i = 0; i < 3; ++i) {
+			sum += squares[2-i][i];
+		}
+	if (Math.abs(sum) === 3)
+			return sum;
+
+	return 0;
+}
 
 
